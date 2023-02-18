cf8c0b8071bab148a300556f4f66efccfec6760c
fededav <67575679+raishid@users.noreply.github.com>
2023-02-18 02:27:03 -0400

    update verson 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyfembed',
-    version='0.1.0',
+    version='0.1.1',
     description='Upload video with the Fembed API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/raishid/pyfembed',
     author='raishid',
     author_email='raishidavid@gmail.com',
